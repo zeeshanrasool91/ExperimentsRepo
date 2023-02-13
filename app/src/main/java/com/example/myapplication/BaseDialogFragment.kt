c3da0196ac653cf04058cce267a53dbdeea9a1f3
zeeshanreasonlabs <zeeshan.rasool@reasonlabs.com>
2023-02-13 14:32:38 +0400

    Updated to canary new ui view

--- app/src/main/java/com/example/myapplication/BaseDialogFragment.kt
+++ app/src/main/java/com/example/myapplication/BaseDialogFragment.kt
@@ -8,8 +8,9 @@ import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatDialogFragment
 import androidx.viewbinding.ViewBinding
 import com.google.android.material.bottomsheet.BottomSheetDialog
+typealias Inflate<T> = (LayoutInflater, ViewGroup?, Boolean) -> T
 
-abstract class BaseDialogFragment<T: ViewBinding>(private val bindingInflater: (LayoutInflater, ViewGroup?, Boolean) -> T)
+open class BaseDialogFragment<T: ViewBinding>(private val inflate: Inflate<T>/*private val bindingInflater: (LayoutInflater, ViewGroup?, Boolean) -> T*/)
     : AppCompatDialogFragment() {
     var _binding: T? = null
     val binding: T get() = _binding ?: error("Must only access binding while fragment is attached.")
@@ -20,7 +21,10 @@ abstract class BaseDialogFragment<T: ViewBinding>(private val bindingInflater: (
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = bindingInflater(inflater, container, false)
+        //_binding = bindingInflater(inflater, container, false)
+        //_binding = bindingInflater.invoke(inflater, container, false)
+        //_binding = inflate.invoke(inflater, container, false)
+        _binding = inflate(inflater, container, false)
         return binding.root
     }
 
@@ -33,4 +37,11 @@ abstract class BaseDialogFragment<T: ViewBinding>(private val bindingInflater: (
         super.onDestroy()
         _binding = null
     }
+
+    companion object {
+        fun <T : ViewBinding> newInstance2(bindingInflater: (LayoutInflater, ViewGroup?, Boolean) -> T): BaseDialogFragment<T> {
+            return BaseDialogFragment(bindingInflater)
+        }
+    }
+
 }
